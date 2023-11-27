ef13e94b4cf04a3e6dafcc0a99d0c003b548d9aa
JoelFrancisco <joelfranciscofilho@gmail.com>
2023-11-27 13:53:07 -0300

    readicionando jedis

--- src/main/java/com/jyula/jyulaapi/JyulaApiApplication.java
+++ src/main/java/com/jyula/jyulaapi/JyulaApiApplication.java
@@ -3,10 +3,16 @@ package com.jyula.jyulaapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.time.Duration;
 
 @EnableWebMvc
 @SpringBootApplication
@@ -24,5 +30,27 @@ public class JyulaApiApplication {
 				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
 			}
 		};
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
+	}
+
+	@Bean
+	public JedisConnectionFactory jedisConnectionFactory() {
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+		redisConfig.setHostName("oregon-redis.render.com");
+		redisConfig.setPort(6379);
+		redisConfig.setPassword("pmV01YGNAM4lLqBmiUJH6D2FBYpJ9UNm");
+		redisConfig.setUsername("red-clccj9bmot1c73df20u0");
+		JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfig = JedisClientConfiguration.builder();
+		jedisClientConfig.connectTimeout(Duration.ofSeconds(60));
+		jedisClientConfig.useSsl();
+
+
+		return new JedisConnectionFactory(redisConfig, jedisClientConfig.build());
 	}
 }
